41d3f0fe558b1f7c5d0c8474bf9bc77d4e534a23
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-12-28 17:51:40 +0100

    Securing the API

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,16 @@
 import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 export default {
-  config(_input) {
-    return {
-      name: "notes",
-      region: 'eu-west-3', // Paris
-    };
-  },
-  stacks(app) {
-    app.stack(StorageStack).stack(ApiStack);
-  }
+    config(_input) {
+        return {
+            name: "notes",
+            region: "eu-west-3", // Paris
+        };
+    },
+    stacks(app) {
+        app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+    },
 } satisfies SSTConfig;
