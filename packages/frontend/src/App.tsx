a27fe7b61f7a40f1646918ecb99e99e60f10307c
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-12-29 10:33:40 +0100

    Added login

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,37 +1,81 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
+
+import { AppContext, AppContextType } from "./lib/contextLib";
+import { onError } from "./lib/errorLib";
 import Routes from "./Routes.tsx";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App container py-3">
-            <Navbar
-                collapseOnSelect
-                bg="light"
-                expand="md"
-                className="mb-3 px-3">
-                <LinkContainer to="/">
-                    <Navbar.Brand className="fw-bold text-muted">
-                        Scratch
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Nav activeKey={window.location.pathname}>
-                        <LinkContainer to="/signup">
-                            <Nav.Link>Signup</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                            <Nav.Link>Login</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Routes />
-        </div>
-    );
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const nav = useNavigate();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (error) {
+      if (error !== "No current user") {
+        onError(error);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+
+    userHasAuthenticated(false);
+
+    nav("/login");
+  }
+
+  return (
+    !isAuthenticating && (
+      <div className="App container py-3">
+        <Navbar
+          collapseOnSelect
+          bg="light"
+          expand="md"
+          className="mb-3 px-3">
+          <LinkContainer to="/">
+            <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider
+          value={{ isAuthenticated, userHasAuthenticated } as AppContextType}>
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
+  );
 }
 
 export default App;
